ba17f1250697d8a46d2a3e9e485570663a23d1e3
Jar'Zeno <jarzenojarrett@gmail.com>
2022-12-22 08:32:33 -0800

    Implment light/dark mode toggle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export class Footer extends React.Component {
             <footer className="dark:bg-zinc-50 bg-zinc-700 flex text-sky-500  dark:text-emerald-500  h-28 items-center justify-center text-4xl">
                 <p>Made By</p>
                 <a className="hover:text-sky-600 dark:hover:text-emerald-600 bg-sky-200 hover:bg-sky-300 dark:bg-emerald-200 dark:hover:bg-emerald-300 border-sky-500 hover:border-sky-600 dark:border-emerald-500 dark:hover:border-emerald-600 rounded-full p-2 border-4 ml-2 mr-2" href="https://github.com/Bantchee">
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg  xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                     </svg>
                 </a>
--- src/components/utility/Toggle.js
+++ src/components/utility/Toggle.js
@@ -1,30 +1,41 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export class Toggle extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isLight: true,
+        }
+
+        this.changeTheme = this.changeTheme.bind(this);
     }
 
-    changeTheme(event) {
+    changeTheme() {
         let root = document.querySelector('html');
 
-        (root.classList.contains('dark')) ? root.classList.remove("dark") : root.classList.add("dark");
+        if (root.classList.contains('dark')) {
+            root.classList.remove("dark")
+            this.setState({
+                isLight: true,
+            });
+        }
+        else {
+            root.classList.add("dark");
+            this.setState({
+                isLight: false,
+            });
+        }
     }
 
     render() {
+        console.log(this.state.isLight, "sdfasd");
         return (
-            <button onClick={this.changeTheme} className="fixed top-0 right-0 mr-4 mt-4 bg-sky-400 p-3 hover:bg-sky-500 rounded-full">
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun">
-                    <circle cx="12" cy="12" r="5" className="stroke-zinc-800"></circle>
-                    <line x1="12" y1="1" x2="12" y2="3" className="stroke-zinc-800"></line>
-                    <line x1="12" y1="21" x2="12" y2="23" className="stroke-zinc-800"></line>
-                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" className="stroke-zinc-800"></line>
-                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" className="stroke-zinc-800"></line>
-                    <line x1="1" y1="12" x2="3" y2="12" className="stroke-zinc-800"></line>
-                    <line x1="21" y1="12" x2="23" y2="12" className="stroke-zinc-800"></line>
-                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" className="stroke-zinc-800"></line>
-                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" className="stroke-zinc-800"></line>
-                </svg>
+            <button onClick={this.changeTheme} className="fixed top-0 right-0 mr-4 mt-4 rounded-full p-3 pt-4 pb-4 bg-sky-400 hover:bg-sky-500 dark:bg-emerald-400 dark:hover:bg-emerald-500 ">
+                <FontAwesomeIcon className=" dark:text-zinc-50 text-zinc-800" icon={(this.state.isLight) ? faSun : faMoon} size="2x" fixedWidth/>
             </button>
         );
     }
