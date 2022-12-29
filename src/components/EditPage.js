6c664b4b913ca3e55bd04b6cbbb19a7ca26df7d8
Jar'Zeno <jarzenojarrett@gmail.com>
2022-12-29 07:44:53 -0800

    Basic Personal Preveiw section

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -5,7 +5,6 @@ import { Education } from './editSections/Education.js';
 
 export class EditPage extends React.Component {
     render() {
-        console.log(this.props);
         return (
             <div className='flex-1 p-6 bg-zinc-200 text-zinc-900 dark:bg-zinc-600 dark:text-zinc-50'>
                 <Personal 
--- src/components/PreviewPage.js
+++ src/components/PreviewPage.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Personal } from "./previewSections/Personal";
 
 export class PreviewPage extends React.Component {
     render() {
-        let {firstName, lastName, role, email, address, phoneNumber, description} = this.props;
+        console.log(this.props);
         return (        
             <div className="flex-1 p-6 bg-zinc-200 text-zinc-900 dark:bg-zinc-600 dark:text-zinc-50">
-                <p>First Name: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Role: {role}</p>
-                <p>Email: {email}</p>
-                <p>Address: {address}</p>
-                <p>Phone Number: {phoneNumber}</p>
-                <p>Description: {description}</p>
+                <Personal 
+                    firstName={this.props.personal.firstName} 
+                    lastName={this.props.personal.lastName} 
+                    role={this.props.personal.role} 
+                    email={this.props.personal.email} 
+                    address={this.props.personal.address} 
+                    phoneNumber={this.props.personal.phoneNumber}
+                    description={this.props.personal.description}  
+                />
             </div> 
         );
     }
