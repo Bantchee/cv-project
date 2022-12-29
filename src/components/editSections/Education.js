3d2b3a653b7a1aab336fd3fccc5154271c9f39f4
Jar'Zeno <jarzenojarrett@gmail.com>
2022-12-29 07:25:44 -0800

    Delete not used code

---
+++ src/components/editSections/Education.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { School } from "../utility/School";
+import { Button } from "../utility/Button";
+
+export class Education extends React.Component {
+    render() {
+        return (
+            <div className="flex flex-col gap-2">
+                <h1>Work Experience</h1>
+                <School onChange={this.props.onChange}/>
+                <Button 
+                    className="bg-green-500 rounded-md"
+                    value="Add" 
+                />
+            </div>
+        );
+    }
+}
