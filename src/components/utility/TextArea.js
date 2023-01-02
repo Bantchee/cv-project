20e785e79b25323de5e38a5a26b5596ab361c88f
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-02 14:47:07 -0800

    Fix dark mode input styling

--- src/components/utility/TextArea.js
+++ src/components/utility/TextArea.js
@@ -5,7 +5,7 @@ export class TextArea extends React.Component {
         let { onChange, name, placeholder, value } = this.props;
         return (
             <textarea 
-                className="rounded pl-2 border-zinc-600 border-solid border-2"
+                className="rounded pl-2 border-zinc-600 border-solid border-2 dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
                 onChange={onChange} 
                 name={name}
                 placeholder={placeholder}
