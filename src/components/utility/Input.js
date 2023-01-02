2bf788ca415a40ef6a1d8f5444a55c733c29bb1b
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-02 14:48:45 -0800

    Remove extra space

--- src/components/utility/Input.js
+++ src/components/utility/Input.js
@@ -6,7 +6,7 @@ export class Input extends React.Component {
         let { onChange, type, name, placeholder, value} = this.props;
         return (
             <input 
-                className="rounded pl-2 border-solid border-2 border-zinc-600  dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
+                className="rounded pl-2 border-solid border-2 border-zinc-600 dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
                 onChange={onChange} 
                 type={type}
                 name={name}
