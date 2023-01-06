d5e7572e5f6fdf52c1838fcf244664a58b33c15d
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-05 17:15:41 -0800

    Refactor Toggle.js into functional component

--- src/components/utility/Toggle.js
+++ src/components/utility/Toggle.js
@@ -1,40 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
-export class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
+export const Toggle = (props) => {
+    const [isLightMode, setIsLightMode] = useState(true);
 
-        this.state = {
-            isLight: true,
-        }
-
-        this.changeTheme = this.changeTheme.bind(this);
-    }
-
-    changeTheme() {
+    const changeTheme = () => {
         let root = document.querySelector('html');
 
         if (root.classList.contains('dark')) {
-            root.classList.remove("dark")
-            this.setState({
-                isLight: true,
-            });
+            root.classList.remove("dark");
+            setIsLightMode(true);
         }
         else {
             root.classList.add("dark");
-            this.setState({
-                isLight: false,
-            });
+            setIsLightMode(false);
         }
-    }
+    };
 
-    render() {
-        return (
-            <button onClick={this.changeTheme} className="fixed top-0 right-0 mr-4 mt-4 rounded-full p-3 pt-4 pb-4 bg-sky-400 hover:bg-sky-500 dark:bg-emerald-400 dark:hover:bg-emerald-500 ">
-                <FontAwesomeIcon className=" dark:text-zinc-50 text-zinc-800" icon={(this.state.isLight) ? faSun : faMoon} size="2x" fixedWidth/>
-            </button>
-        );
-    }
+    return (
+        <button onClick={changeTheme} className="fixed top-0 right-0 mr-4 mt-4 rounded-full p-3 pt-4 pb-4 bg-sky-400 hover:bg-sky-500 dark:bg-emerald-400 dark:hover:bg-emerald-500 ">
+            <FontAwesomeIcon className=" dark:text-zinc-50 text-zinc-800" icon={isLightMode ? faSun : faMoon} size="2x" fixedWidth/>
+        </button>
+    );
 }
