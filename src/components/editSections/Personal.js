3e7e580f8b2bebcfbf56e63816bdb3227ed713db
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-05 17:23:31 -0800

    Refactor ./src/components/editSections/Personal.js into functional component

--- src/components/editSections/Personal.js
+++ src/components/editSections/Personal.js
@@ -1,63 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input } from '../utility/Input';
 import { TextArea } from '../utility/TextArea';
 
-export class Personal extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Personal Information</h1>
-                <div className="flex flex-col gap-2 dark:text-zinc-900">
-                    <Input
-                        onChange={(e) => this.props.onChange(e)} 
-                        type="text"
-                        name="firstName"
-                        placeholder="First Name"
-                        value = {this.props.personal.firstName}
-                    />
-                    <Input
-                        onChange={(e) => this.props.onChange(e)} 
-                        type="text"
-                        name="lastName"
-                        placeholder="Last Name"
-                        value = {this.props.personal.lastName}
-                    />
-                    <Input
-                        onChange={(e) => this.props.onChange(e)} 
-                        type="text"
-                        name="role"
-                        placeholder="Role"
-                        value = {this.props.personal.role}
-                    />
-                    <Input
-                        onChange={(e) => this.props.onChange(e)} 
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value = {this.props.personal.email}
-                    />
-                    <Input
-                        onChange={(e) => this.props.onChange(e)} 
-                        type="text"
-                        name="address"
-                        placeholder="Address"
-                        value = {this.props.personal.address}
-                    />
-                    <Input
-                        onChange={(e) => this.props.onChange(e)} 
-                        type="text"
-                        name="phoneNumber"
-                        placeholder="Phone Number"
-                        value = {this.props.personal.phoneNumber}
-                    />
-                    <TextArea 
-                        onChange={(e) => this.props.onChange(e)} 
-                        name="description"
-                        placeholder="Description"
-                        value = {this.props.personal.description}
-                    />
-                </div>
+export const Personal = (props) => {
+    return (
+        <div>
+            <h1>Personal Information</h1>
+            <div className="flex flex-col gap-2 dark:text-zinc-900">
+                <Input
+                    onChange={(e) => props.onChange(e)} 
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"
+                    value = {props.personal.firstName}
+                />
+                <Input
+                    onChange={(e) => props.onChange(e)} 
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    value = {props.personal.lastName}
+                />
+                <Input
+                    onChange={(e) => props.onChange(e)} 
+                    type="text"
+                    name="role"
+                    placeholder="Role"
+                    value = {props.personal.role}
+                />
+                <Input
+                    onChange={(e) => props.onChange(e)} 
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value = {props.personal.email}
+                />
+                <Input
+                    onChange={(e) => props.onChange(e)} 
+                    type="text"
+                    name="address"
+                    placeholder="Address"
+                    value = {props.personal.address}
+                />
+                <Input
+                    onChange={(e) => props.onChange(e)} 
+                    type="text"
+                    name="phoneNumber"
+                    placeholder="Phone Number"
+                    value = {props.personal.phoneNumber}
+                />
+                <TextArea 
+                    onChange={(e) => props.onChange(e)} 
+                    name="description"
+                    placeholder="Description"
+                    value = {props.personal.description}
+                />
             </div>
-        )
-    }
+        </div>
+    )
 }
