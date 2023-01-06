766019375836d9d17bff2b7290c4bcbdf3c76ebd
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-05 17:11:08 -0800

    Refactor TextArea.js into functional component

--- src/components/utility/TextArea.js
+++ src/components/utility/TextArea.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-export class TextArea extends React.Component {
-    render() {
-        let { onChange, name, placeholder, value } = this.props;
-        return (
-            <textarea 
-                className="rounded pl-2 border-zinc-600 border-solid border-2 dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
-                onChange={onChange} 
-                name={name}
-                placeholder={placeholder}
-                // value={value}
-            />
-        )
-    }
+export const TextArea = (props) => {
+    let { onChange, name, placeholder, value } = props;
+    return (
+        <textarea 
+            className="rounded pl-2 border-zinc-600 border-solid border-2 dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
+            onChange={onChange} 
+            name={name}
+            placeholder={placeholder}
+            // value={value}
+        />
+    )
 }
