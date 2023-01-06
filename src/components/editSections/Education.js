a3607938b06aa73620bf72e451b391a09f0a203d
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-05 17:25:46 -0800

    Refactor ./src/components/editSections/Education.js into functional component

--- src/components/editSections/Education.js
+++ src/components/editSections/Education.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { School } from "../utility/edit/School";
 
-export class Education extends React.Component {
-    render() {
-        return (
-            <div className="flex flex-col gap-2">
-                <h1>Education</h1>
-                {
-                    this.props.education.schools.map((school, index) => {
-                        return (
-                            <School
-                                key={index} 
-                                onChange={(e) => this.props.onChange(e, index)}
-                                onClick={() => this.props.deleteOnClick(index)}
-                            />
-                        )
-                    })
-                }
-                <button 
-                    className="bg-green-500 rounded-md"
-                    onClick={() => this.props.addOnClick()}
-                >Add</button>
-            </div>
-        );
-    }
+export const Education = (props) => {
+    return (
+        <div className="flex flex-col gap-2">
+            <h1>Education</h1>
+            {
+                props.education.schools.map((school, index) => {
+                    return (
+                        <School
+                            key={index} 
+                            onChange={(e) => props.onChange(e, index)}
+                            onClick={() => props.deleteOnClick(index)}
+                        />
+                    )
+                })
+            }
+            <button 
+                className="bg-green-500 rounded-md"
+                onClick={() => props.addOnClick()}
+            >Add</button>
+        </div>
+    );
 }
