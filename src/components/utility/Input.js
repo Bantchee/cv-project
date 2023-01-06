bdf474f0ac701860fc7821f9016d51efe1231d17
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-05 17:09:55 -0800

    Refactor Input.js into functional component

--- src/components/utility/Input.js
+++ src/components/utility/Input.js
@@ -1,18 +1,15 @@
 import React from 'react'
 
-export class Input extends React.Component {
-    shouldComponentUpdate = () => false;
-    render() {
-        let { onChange, type, name, placeholder, value} = this.props;
-        return (
-            <input 
-                className="rounded pl-2 border-solid border-2 border-zinc-600 dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
-                onChange={onChange} 
-                type={type}
-                name={name}
-                placeholder={placeholder}
-                // value={value}
-            />
-        )
-    }
+export const Input = (props) => {
+    let { onChange, type, name, placeholder, value} = props;
+    return (
+        <input 
+            className="rounded pl-2 border-solid border-2 border-zinc-600 dark:border-zinc-200 dark:bg-zinc-800 dark:text-zinc-50"
+            onChange={onChange} 
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            // value={value}
+        />
+    )
 }
