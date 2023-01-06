92caaf4dfa24b0e0ecad39b689a6955d90e5901e
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-05 17:24:38 -0800

    Refactor ./src/components/editSections/Experience.js into functional component

--- src/components/editSections/Experience.js
+++ src/components/editSections/Experience.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { Career } from "../utility/edit/Career.js";
 
-export class Experience extends React.Component {
-    render() {
-        return (
-            <div className="flex flex-col gap-2">
-                <h1>Work Experience</h1>
-                {
-                    this.props.experience.careers.map((career, index) => {
-                        return (
-                            <Career
-                                key={index} 
-                                onChange={(e) => this.props.onChange(e, index)}
-                                onClick={() => this.props.deleteOnClick(index)}
-                            />
-                        )
-                    })
-                }
-                <button 
-                    className="bg-green-500 rounded-md"
-                    onClick={() => this.props.addOnClick()}
-                >Add</button>
-            </div>
-        );
-    }
+export const Experience = (props) => {
+    return (
+        <div className="flex flex-col gap-2">
+            <h1>Work Experience</h1>
+            {
+                props.experience.careers.map((career, index) => {
+                    return (
+                        <Career
+                            key={index} 
+                            onChange={(e) => props.onChange(e, index)}
+                            onClick={() => props.deleteOnClick(index)}
+                        />
+                    )
+                })
+            }
+            <button 
+                className="bg-green-500 rounded-md"
+                onClick={() => props.addOnClick()}
+            >Add</button>
+        </div>
+    );
 }
